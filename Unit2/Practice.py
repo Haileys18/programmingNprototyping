706b683b8aae2a3a0ad4e5392b80c2af2f429ccd
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-09-23 12:34:24 -0400

    Update Practice.py

--- Unit2/Practice.py
+++ Unit2/Practice.py
@@ -15,3 +15,14 @@ r = int(input("What is the radius of your radius?"))
 h = int(input("What is the radius of your height?"))
 V = math.pi*r**2*h
 print("V=" ,V)
+
+
+import math 
+print("What are the factors of 2x^2 + 6x - 20")
+a = int(input("What is the a value: "))
+b = int(input("What is the b value: "))
+c = int(input("What is the c value: "))
+x =int((-b+ math.sqrt(b**2-(4*a*c)))/(2*a))
+x2 =int((-b- math.sqrt(b**2-(4*a*c)))/(2*a))
+print("The first root is: " +str(x))
+print("The second root is: "+ str(x2))
