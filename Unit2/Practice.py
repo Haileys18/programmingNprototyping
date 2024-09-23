4256474324bea556189bc2ac1d66a6194c2412ca
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-09-23 11:53:15 -0400

    Create Practice.py

---
+++ Unit2/Practice.py
@@ -0,0 +1,5 @@
+#Getting the length from the user
+length= int(input("What is the length of the rectangle?"))
+width= int(input("What is the width of the rectangle?"))
+perimeter = 2 * (length + width)
+print("The perimeter is " + str(perimeter))
