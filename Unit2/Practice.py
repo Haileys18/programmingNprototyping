35d94acbd593c1aa47318214b9c3419e6833b183
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-09-23 12:02:11 -0400

    Update Practice.py

--- Unit2/Practice.py
+++ Unit2/Practice.py
@@ -3,3 +3,9 @@ length= int(input("What is the length of the rectangle?"))
 width= int(input("What is the width of the rectangle?"))
 perimeter = 2 * (length + width)
 print("The perimeter is " + str(perimeter))
+
+#important math
+r = int(input("What is the radius of your cirle?"))
+C = 2*r*math.pi
+A = math.pi*r**2
+print("C=" ,C," A=" ,A)
