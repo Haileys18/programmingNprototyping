5a41d6f9b1f6d1cfc90bdfb0bae61d4d2efa8ce1
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-09-24 12:02:35 -0400

    Create CFU2.py

---
+++ Unit1/CFU2.py
@@ -0,0 +1,13 @@
+#Date : 9/24/24
+#Period 5-6
+#Hailey Sooknanan
+#asks a user for for their first name and last name and greets them by both their first and last name.  
+# CF2
+name1 = input("What is your first name?")
+name2 = input("What is your last name?")
+age = int(input("What is your age"))
+question = int(input("How many periods do u have at school?"))
+question1 = int(input("How long until you turn 18?"))
+print(name1 +" "+ name2+ "\nyou are "+ str(age)+" years old"+
+      "\nand you have " +str(question)+" periods" +"!" +
+      "\nYou have "+ str(question1)+" years until your 18!")
