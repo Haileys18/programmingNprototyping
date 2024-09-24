5afda40ddb9f07ba8b1ab3a65c686f5785937933
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-09-24 11:56:56 -0400

    Create CFU1.py

---
+++ Unit1/CFU1.py
@@ -0,0 +1,9 @@
+#Date : 9/24/24
+#Period 5-6
+#Hailey Sooknanan
+#Creating a variables for first and last name
+# CF1
+f_name= input("Please enter first name: ")
+l_name = input("Please enter last name: ")
+name = f_name +" "+ l_name
+print("Hello there, " + name +"!")
