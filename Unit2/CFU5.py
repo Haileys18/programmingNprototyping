2da6f280983ef2f54e590d72b97820b933bbc519
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-01 11:30:44 -0400

    Update CFU5.py

--- Unit2/CFU5.py
+++ Unit2/CFU5.py
@@ -1,1 +1,19 @@
+#Variables for user to input amount of each coin
+pennies = int(input("How many pennies do you have?"))
+nickels = int(input("How many nickels do you have?"))
+dimes = int(input("How many dimes do you have?"))
+quarters = int(input("How many quarters do you have?"))
+toonies = int(input("How many toonies do you have?"))
+loonies = int(input("How many loonies do you have?"))
 
+#outputs the amount of coins for each
+print("pennies:",pennies)
+print("nickels:",nickels)
+print("Dimes:",dimes)
+print("Quarters:", quarters)
+print("Toonies:",toonies)
+print("Loonies:",loonies)
+
+total_value = (pennies + 5*nickels + 10*dime + 25*quarters + 100*loonies + 200*toonies)/100
+
+print("Total value of coins: $" + "{:.2f}".format(total_value))
