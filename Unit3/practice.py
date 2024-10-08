ae40469d69af087c5d4fc03dfdc6edf7f7d75d60
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-08 12:22:02 -0400

    Update practice.py

--- Unit3/practice.py
+++ Unit3/practice.py
@@ -19,3 +19,8 @@ def HappyBday(name):
 HappyBday("Cersei")
 HappyBday("Jamie")
 HappyBday("Luna")
+def mult_2_nums(num1 , num2): #parameter
+    print(num1 * num2)
+    
+mult_2_nums(5,4) #arguments
+mult_2_nums(10,-3.5)
