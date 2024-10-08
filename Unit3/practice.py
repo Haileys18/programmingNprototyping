c15520f5b16c4979435fac361a110203e399e22a
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-08 12:06:15 -0400

    Create practice.py

---
+++ Unit3/practice.py
@@ -0,0 +1,15 @@
+def mult_function(x,y):
+    z=x*y
+    print("I heart math")
+    print(z)
+    return z
+   
+mult_function(5,4)
+mult_function(7,3)
+mult_function(100,750)
+
+def my_first_func():
+    print("Testing...")
+    print("It works!")
+    
+my_first_func()
