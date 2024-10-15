700c0b15b0a9ffdf3e054a1538da1e7b9cde443a
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-15 11:28:41 -0400

    Create CFU8.py

---
+++ Unit3/CFU8.py
@@ -0,0 +1,26 @@
+#V!
+#Hailey Sooknanan
+#5-6
+#10/15
+#Grubhub deliveries asking user the order
+word = input("Did you order delivery?")
+
+if word == "yes":
+    print("Great!")
+else:
+    print("NO?! So who is cooking?")
+
+#V2
+#Hailey Sooknanan
+#5-6
+#10/15
+#Grubhub deliveries asking user the order
+word = input("Did you order delivery?")
+
+if word == "yes":
+    cost = int(input("What is the cost?"))
+    people = int(input("How many people"))
+    total = cost/people
+    print("total per person: " + str(total) )
+else:
+    print("NO?! So who is cooking?")
