8a844df616e88df36a4724d232be1252dbe88d75
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-18 12:48:30 -0400

    Update CFU9.py

--- Unit3/CFU9.py
+++ Unit3/CFU9.py
@@ -3,30 +3,28 @@
 #10/17
 #Ask user to guess what number will be rolled
 import random
+num_rolls = int(input("Let's play dice! How many rolls do you want to play?"))
 
-def roll_dice():
-    score = 0
-    # Ask user how many times they want to roll the dice
-    plays = int(input("How many rolls do you want to play? "))
-    print("Amount of rolls: " + str(plays))
+
+def guess(total):
+     user_guess = int(input("What number do u think you rolled?"))
+     random_num = random.randint(1,6)
+     roll = random_num
+     print("You guessed: " + str(user_guess) + " Dice rolled: " + str(roll))
+     if user_guess == roll:
+        total += 6
+     else:
+        total -= 1
+        return total
+
+def rolls(num_rolls, total = 0):
+    if num_rolls == 0:
+        return total
+    else:
+        total = guess(total)
+        return rolls(num_rolls-1, total)
+
+total = 0
+final_total = rolls(num_rolls, total)
+print("Your score is: " + str(final_total))
     
-    # Loop for the number of plays
-    for roll in range(plays):
-        x = random.randint(1, 6)  # Generate a random number between 1 and 6
-        guess = int(input(f"Roll {roll + 1} - Guess a number from 1-6: "))
-        print("Dice Rolled!!: " + str(x))
-        print("Your Guess!!: " + str(guess))
-        
-        # Compare guess with the random dice roll
-        if guess == x:
-            print("You guessed correct!!")
-            score - score + 6  # Add 6 points for a correct guess
-        else:
-            print("You guessed wrong.")
-            score = score - 1  # Subtract 1 point for a wrong guess
-
-    # Print final score after all rolls
-    print(f"Final Score: {score}")
-
-# Run the game
-roll_dice()
