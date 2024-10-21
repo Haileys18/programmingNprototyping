84d1f7f5249b644fee09b40eb04927e6f42f0bb7
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-21 12:32:21 -0400

    Update CFU9.py

--- Unit3/CFU9.py
+++ Unit3/CFU9.py
@@ -1,30 +1,24 @@
-#Hailey Sooknanan
-#5-6
-#10/17
-#Ask user to guess what number will be rolled
+#Hailey sooknanan
+#Period-5-6
+#10/21
+#Have the user guess the generated number, give them hints if it is to high or 
+#to low while the amount of attempts increase and prints when they are correct
 import random
-num_rolls = int(input("Let's play dice! How many rolls do you want to play?"))
-
-
-def guess(total):
-     user_guess = int(input("What number do u think you rolled?"))
-     random_num = random.randint(1,6)
-     roll = random_num
-     print("You guessed: " + str(user_guess) + " Dice rolled: " + str(roll))
-     if user_guess == roll:
-        total += 6
-     else:
-        total -= 1
-        return total
-
-def rolls(num_rolls, total = 0):
-    if num_rolls == 0:
-        return total
-    else:
-        total = guess(total)
-        return rolls(num_rolls-1, total)
-
-total = 0
-final_total = rolls(num_rolls, total)
-print("Your score is: " + str(final_total))
+random_num = random.randint(1,10)
+    
+  
+def guess_function(attempt):
+    
+    user_guess = int(input("Guess a number between 1-10"))
+    attempt += 1
+    if user_guess == random_num:
+        print("Correct!! Attempts: " + str(attempt))
+    elif user_guess > random_num:
+        print("Too High")
+        guess_function(attempt)
+    else: 
+        print("Too Low")
+        guess_function(attempt)
+        
+guess_function(0)
     
