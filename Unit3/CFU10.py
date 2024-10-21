379a632470f870182e3dc1f9bafc83b16046f447
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-21 12:38:56 -0400

    Update CFU10.py

--- Unit3/CFU10.py
+++ Unit3/CFU10.py
@@ -1,16 +1,24 @@
 #Hailey sooknanan
 #Period-5-6
 #10/21
-#Have the user guess the generated number, give them hints if it is to high or to low
-
-def random_function():
-    random_num = random.randint(1,50)
-
-def input_function():
-    user_guess = int(input("Guess a number between 1-50"))
+#Have the user guess the generated number, give them hints if it is to high or 
+#to low while the amount of attempts increase and prints when they are correct
+import random
+random_num = random.randint(1,10)
+    
   
-def guess_function():
-    attempts=0
-    if input_function == random_function:
-        attempts += 1
+def guess_function(attempt):
+    
+    user_guess = int(input("Guess a number between 1-10"))
+    attempt += 1
+    if user_guess == random_num:
+        print("Correct!! Attempts: " + str(attempt))
+    elif user_guess > random_num:
+        print("Too High")
+        guess_function(attempt)
+    else: 
+        print("Too Low")
+        guess_function(attempt)
+        
+guess_function(0)
     
