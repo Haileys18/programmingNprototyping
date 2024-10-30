b197d2cea341e01dd53135be984afa8c48880158
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-10-30 11:28:20 -0400

    Update CFU12.py

--- Unit 4/CFU12.py
+++ Unit 4/CFU12.py
@@ -4,21 +4,41 @@
 # Asking the user what the password is and if they get it wrong, gives them
 # three more tries with one hint per try
 
-guess = 0
-user_guess = ""
 
-while user_guess != "simonsnyc" and guess < 3:
-    user_guess = input("Enter the password: ")
-    guess += 1
+def version1():
+    password = "simonsnyc"
+    user_pass = input("Enter the password")
+    # loop if the password is incorrect
+    while password != user_pass:
+        print("Incorrect password! try again")
+        user_pass = input("Enter the password")
+#print this when correct password is entered
+    print("Correct password! You may enter...")
+def version2():
     
-    if user_guess == "simonsnyc":
-        print("Correct! You may enter….")
-    elif guess == 1:
-        print("Hint: one word connected with 's'.")
-    elif guess == 2:
-        print("Hint: blue chipmunk from Alvin and the Chipmunks.")
-    elif guess == 3:
-        print("Hint: The city that has five boroughs.")
+	guess = 0
+	user_guess = ""
 
-if user_guess != "simonsnyc":
-    print("You have exceeded the maximum number of attempts.")
+	while user_guess != "simonsnyc" and guess < 3:
+    	user_guess = input("Enter the password: ")
+    	guess += 1
+    
+    	if user_guess == "simonsnyc":
+            print("Correct! You may enter….")
+    	elif guess == 1:
+        	print("Hint: one word connected with 's'.")
+    	elif guess == 2:
+        	print("Hint: blue chipmunk from Alvin and the Chipmunks.")
+    	elif guess == 3:
+        	print("Hint: The city that has five boroughs.")
+
+	if user_guess != "simonsnyc":
+    	print("You have exceeded the maximum number of attempts.")
+    
+userpick = int(input("Choose a function 1 or 2"))
+if userpick == 1:
+    version1()
+elif userpick == 2:
+    version2()
+else:
+    print("Choose a function")
