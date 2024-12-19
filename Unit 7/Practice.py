b4cfc96c5e6fe8bd8766048812ff8b5f1a39d76c
S. Hailey <144722264+Haileys18@users.noreply.github.com>
2024-12-19 12:44:36 -0500

    Create Practice.py

---
+++ Unit 7/Practice.py
@@ -0,0 +1,12 @@
+#Hailey Sooknanan
+#Period 5-6
+#12/19
+#Unit 7-Lists
+favorites= ["sushi","red","for loop", "18"]
+print(favorites[0])
+groceries=["Eggs","Apples","Milk","Bread","Tumeric"]
+print(groceries[4])
+people=["Nibrus", "Marisa", "Evelyn", "Sharena", "Mia", "Malachi", "Emirah", "Fahim", "Arafath", "Edwin", "Harbans", "Syeda", "Grace", "Amelia", "Sheldon", "Angel", "Farhan", "Sarika", "Aarya", "Romina","Nafisa", "Cesar", "William"]
+donation =["Cupcakes", "Cookies","Candy", "Plates and Untensils", "Nothing", "Drinks", "Food", "Nothing", "Chips", "Scooby Snacks","Choc Chip Cookies", "Sour Patch", "Pretzels", "Chips", "Donuts", "Nothing", "Nothing", "Nothing", "Chips", "Nothing", "Nothing", "Nothing"]
+for i in range(22):
+    print(f"{people[i]} will bring {donation[i]}.")
